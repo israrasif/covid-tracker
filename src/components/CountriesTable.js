7c60fec84ee335361888d9c508b58dba425e40de
israrasif <68469832+israrasif@users.noreply.github.com>
2021-06-22 22:36:46 +0530

    remove warnings

--- src/components/CountriesTable.js
+++ src/components/CountriesTable.js
@@ -5,7 +5,6 @@ import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { Box } from '@material-ui/core';
